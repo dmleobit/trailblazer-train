48df379b066561289123e5d7f5608531eedd5f1f
Molodko Dmytro <dm@leobit.com>
2021-06-16 13:09:14 +0300

    Use Nested macro

--- app/concepts/post/operation/like.rb
+++ app/concepts/post/operation/like.rb
@@ -1,20 +1,5 @@
 module Post::Operation
   class Like < Trailblazer::Operation
-    pass :destroy_like, fast_track: true
-    step :create_like
-
-    def destroy_like(ctx, user:, origin:, **)
-      if origin.likes.find_by(user: user)&.destroy
-        ctx[:message] = 'Disliked'
-        ctx[:flash_type] = 'alert'
-        return Trailblazer::Activity::FastTrack::PassFast
-      end
-    end
-
-    def create_like(ctx, user:, origin:, **)
-      origin.likes.create(user: user)
-      ctx[:message] = 'Liked'
-      ctx[:flash_type] = 'notice'
-    end
+    step Nested(Shared::Operation::Like), fast_track: true
   end
 end
