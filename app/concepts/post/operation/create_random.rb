660d6a3993a0b54e9b619b2008e14d4d4a5dabcb
Molodko Dmytro <dm@leobit.com>
2021-06-16 14:08:07 +0300

    Added wrap transaction

--- app/concepts/post/operation/create_random.rb
+++ app/concepts/post/operation/create_random.rb
@@ -1,8 +1,14 @@
 module Post::Operation
   class CreateRandom < Trailblazer::Operation
     step Model(Post, :new)
-    pass :assign_attributes
-    step :save
+    # same as pass :assign_attributes. Will go to success track even in case of failure
+    step :assign_attributes, Output(:failure) => Track(:success) # Track or End
+
+    step Wrap( Shared::CallableTransaction ) {
+      step :save
+      # Just to show how transactions works in separate steps
+      step :create_one_new
+    }
     fail :log_error
     step :log
 
@@ -14,7 +20,13 @@ module Post::Operation
     end
 
     def save(ctx, model:, **)
-      model.save
+      model.save!
+    end
+
+    def create_one_new(ctx, user:, **)
+      # First line raise an error
+      # Post.create!()
+      Post.create!(user: user, text: Faker::Lorem.sentence)
     end
 
     def log_error(ctx, model:, **)
